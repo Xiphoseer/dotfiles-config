178745e9e0739320394699ac6b03acf5765298d0
Daniel Schweighöfer <SchweighoeferD@gmail.com>
2018-10-10 23:52:13 +0200

    Add zlist, zset

--- fish/completions/z.fish
+++ fish/completions/z.fish
@@ -1,11 +1,1 @@
-function __fish_z_bookmark
-  for val in $argv
-    echo -e $val\t(readlink (echo $__fish_bookmark_dir)$val)
-  end
-end
-
-function __fish_z_bookmarks
-  __fish_z_bookmark (ls -1 (echo $__fish_bookmark_dir))
-end
-
-complete -c z -fa '(__fish_z_bookmarks)'
+complete -c z -fa '(zlist)'
--- fish/conf.d/z.fish
+++ fish/conf.d/z.fish
@@ -1,5 +1,30 @@
 set -g __fish_bookmark_dir $HOME/.local/share/bookmarks/
 
+function zlist
+  if test (count $argv) -eq 0
+    set -l files (ls -1 (echo $__fish_bookmark_dir))
+    if test (count $files) -ne 0
+      zlist $files
+    end
+  else
+    for val in $argv
+      echo -e $val\t(readlink (echo $__fish_bookmark_dir)$val)
+    end
+  end
+end
+
 function z
-  cd (readlink $HOME/.local/share/bookmarks/$argv)
+  if test (count $argv) -eq 1
+    cd (readlink $HOME/.local/share/bookmarks/$argv)
+  else
+    echo "Usage: z <name>"
+  end
+end
+
+function zset
+  if test (count $argv) -eq 2
+    ln -s (realpath $argv[1]) $__fish_bookmark_dir$argv[2]
+  else
+    echo "Usage: zset <target> <name>"
+  end
 end
