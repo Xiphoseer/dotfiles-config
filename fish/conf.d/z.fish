eee52a68f1c1466a4052259e7e974e399443cc0c
Daniel Schweighöfer <SchweighoeferD@gmail.com>
2018-10-12 12:59:13 +0200

    Change zlist to find
    - limit to finding symlinks
    - make subfolders work

--- fish/conf.d/z.fish
+++ fish/conf.d/z.fish
@@ -1,8 +1,17 @@
 set -g __fish_bookmark_dir $HOME/.local/share/bookmarks/
 
+function __zlist_find
+  set -l __fish_bookmark_dir_c (echo $__fish_bookmark_dir | grep -o "/" | wc -l)
+  find $__fish_bookmark_dir -type l | cut -sd / -f (math $__fish_bookmark_dir_c + 1)-
+end
+
+function __zlist_ls
+  ls -1 (echo $__fish_bookmark_dir)
+end
+
 function zlist
   if test (count $argv) -eq 0
-    set -l files (ls -1 (echo $__fish_bookmark_dir))
+    set -l files (__zlist_find)
     if test (count $files) -ne 0
       zlist $files
     end
